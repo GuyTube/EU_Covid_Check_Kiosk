bc809ac40f941b344cb32a5510d577e16906a376
PMorelNowina <philippe.morel@nowina.lu>
2021-10-10 09:27:35 +0000

    First functional app. Need more refactoring.

---
+++ checker/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS thilab_users;
+
+CREATE TABLE thilab_users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  surname TEXT NOT NULL,
+  dateOfBirth TEXT NOT NULL,
+  isMember INTEGER NOT NULL
+);
+
